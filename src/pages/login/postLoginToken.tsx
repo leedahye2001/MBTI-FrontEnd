c849e9e9eecac238d845019b3aada2f4c9d67edf
edmund <edmund1000@naver.com>
2023-07-27 09:18:56 +0900

    chore :commit  to resolve postLoginToken error

--- src/pages/login/postLoginToken.tsx
+++ src/pages/login/postLoginToken.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const postLoginToken = async (idToken: string): Promise<boolean> => {
-  const API_URL = process.env.REACT_APP_API_URL;
+  const API_URL = 'http://localhost:8080';
   const path = '/v1/oauth/login';
 
   try {
